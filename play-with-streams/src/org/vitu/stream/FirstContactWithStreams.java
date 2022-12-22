54d0ef78cb17a8d1349bab55c2add4114e8e6798
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2022-12-22 16:57:06 +0100

    Added third stream source type example. Created txt file with lines for next stream source entry

--- play-with-streams/src/org/vitu/stream/FirstContactWithStreams.java
+++ play-with-streams/src/org/vitu/stream/FirstContactWithStreams.java
@@ -15,10 +15,15 @@ public class FirstContactWithStreams {
 		
 		// Stream sur un tableau
 		String[] arrayOfStrings = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
-		// arrayOfStrings.stream(); --> ne fonctionne pas, car la méthode stream() ne s'applique pas à ce type d'objets. 
+		// arrayOfStrings.stream(); --> ne fonctionne pas, car la methode stream() ne s'applique pas sur ce type d'objets. 
 		System.out.println("\nStream sur un tableau");
 		Arrays.stream(arrayOfStrings).forEach(System.out::println);
 		
+		// Stream a partir d'elements
+		System.out.println("\nStream sur des élémnets");
+		Stream.of("one", "two", "three", "four").forEach(System.out::println);
+		
+		// Stream sur les lignes d'un fichier (repertoire files cree a la racine du projet et non dans le repertoire source)
 		
 	
 	}
