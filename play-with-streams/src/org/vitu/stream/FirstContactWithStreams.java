368b3487981a0a8e9bd21d3a796b7b2b79a6a18e
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2022-12-22 16:49:58 +0100

    Added Stream examples in the first contact class

--- play-with-streams/src/org/vitu/stream/FirstContactWithStreams.java
+++ play-with-streams/src/org/vitu/stream/FirstContactWithStreams.java
@@ -1,5 +1,25 @@
 package org.vitu.stream;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class FirstContactWithStreams {
-// hello
+	
+	public static void main(String... args) {
+		
+		// Stream sur une liste
+		List<String> strings = List.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
+		System.out.println("Stream sur une liste");
+		strings.stream().forEach(System.out::println);
+		
+		// Stream sur un tableau
+		String[] arrayOfStrings = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+		// arrayOfStrings.stream(); --> ne fonctionne pas, car la méthode stream() ne s'applique pas à ce type d'objets. 
+		System.out.println("\nStream sur un tableau");
+		Arrays.stream(arrayOfStrings).forEach(System.out::println);
+		
+		
+	
+	}
 }
