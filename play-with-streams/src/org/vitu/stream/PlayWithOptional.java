a004c005ff517e66d1e81affd827ddbfb1f1dc83
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2022-12-23 10:21:45 +0100

    Added and created play with optional

--- play-with-streams/src/org/vitu/stream/PlayWithOptional.java
+++ play-with-streams/src/org/vitu/stream/PlayWithOptional.java
@@ -1,8 +1,25 @@
 package org.vitu.stream;
 
+import java.util.Optional;
+
 public class PlayWithOptional {
 	
 	public static void maint(String[] args) {
+		// Quelle est la difference entre int10 et integer10 ?
+		// int10 est un type primitif sur lequel nous n'avons pas de methodes
+		// integer10 n'est pas un type primitif, Integer est une classe "wrapper", car c'est une classe qui enveloppe une valeur, ici 10, et qui permet de rajouter des methodes sur cette valeur
+		// Donc l'objet de la classe Integer est d'avoir acces a des methodes
+		Integer integer10 = Integer.valueOf(10);
+		int int10 = 10;
 		
+		
+		// Un optional est une classe wrapper, et ici elle enveloppe la valeur "one"
+		// La difference avec la classe Integer, est qu'avec optional, nous pouvons ne pas avoir de valeur enveloppee alors qu'Integer doit systematiquement envelopper une valeur
+		Optional<String> opt = Optional.of("one");
+		Optional<String> emptyOpt = Optional.empty();
+	
+		boolean empty = opt.isEmpty();
+		System.out.println("Optional vide : " + empty);
+		System.out.println("Optional vide : " + emptyOpt.isEmpty());
 	}
 }
