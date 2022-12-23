0ba706441613416af20f9ca5f8838f8910bea3bd
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2022-12-23 18:38:30 +0100

    Used Pattern to split file lines and used map and flatmap

--- play-with-streams/src/org/vitu/stream/PlayWithMapFilterReduce.java
+++ play-with-streams/src/org/vitu/stream/PlayWithMapFilterReduce.java
@@ -1,11 +1,17 @@
 package org.vitu.stream;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class PlayWithMapFilterReduce {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		List<String> strings = List.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
 		
@@ -19,7 +25,71 @@ public class PlayWithMapFilterReduce {
 		
 		System.out.println("Longueur max = " + maxLength); // Retourne 5
 		
-		// Maintenant que se passe t'il si on reprends le code qui permet de lire le fichier
+		// Maintenant que se passe t'il si on reprends le code qui permet de lire le fichier (en reprenant du code de la class FirstContactWithStream
+		String fileName = "files/lines.txt";
+		Path path = Path.of(fileName);
+		
+		// Maintenant si on cherche un max entier : 
+		int maxLengthLine =
+		Files.lines(path)
+			.mapToInt(String::length) // En passant mapToInt() cela nous retournera une IntStream() au lieu de map(), ca nous evite de passer un comparateur dans max()
+			.max() // Car nous savons comment comparer des entiers de facon naturelle
+			.orElseThrow();	
+		System.out.println("Longueur max = " + maxLengthLine); // Nous donne la longueur de la ligne la plus longue (20) et non pas la ligne la plus longue
+		
+		// Et si nous cherchons un max de chaine de caracteres :
+		String maxLineByLength =
+		Files.lines(path)
+			.max(Comparator.comparing(line -> line.length()))
+			.orElseThrow(); // le max de string retourne un optional donc nous devons utiliser orElseThrow()
+		System.out.println("Ligne de longueur maximale = " + maxLineByLength); // retourne "five six seven eight"
+		
+		// Pour le moment nous avons 4 lignes, donc 4 elements dans notre Stream<>
+		long count = Files.lines(path).count();
+		System.out.println("Count = " + count);
+		
+		// Mais nous voudrions avoir 12 elements dans notre Stream<>, nous pouvons utiliser la technique pattern
+		System.out.println("\nStream sur une ligne découpée par un pattern");
+		
+		Function<String, Stream<String>> split = 
+			l -> Pattern.compile(" ").splitAsStream(l);
+		
+		String line = "one two three four";
+		Stream<String> stream = split.apply(line);
+// 			Pattern.compile(" ")
+// 				.splitAsStream(line);
+		stream.forEach(System.out::println);
+		
+		// Ici nous avons mis le pattern de split dans une fonction, et l'appliquons sur la ligne que nous voulons
+		Stream<Stream<String>> streamOfStream =
+		Files.lines(path) // Retourne un Stream<String>
+			.map(split); // Stream<Stream<String>>
+		
+		long count2 = streamOfStream.count();
+		System.out.println("Count = " + count2); // Nous obtenons toujours 4, car la methode map() ne modifie jamais le nombre d'elements du Stream<> sur lequel elle opere
+		
+		Files.lines(path)
+			.map(split)
+			.forEach(System.out::println);
+		// Retourne :
+		// java.util.stream.ReferencePipeline$Head@66d33a
+		// java.util.stream.ReferencePipeline$Head@7cf10a6f
+		// java.util.stream.ReferencePipeline$Head@7e0babb1
+		// java.util.stream.ReferencePipeline$Head@6debcae2 --> donc les 4 elements retournes sont bien des Stream car c'est l'interface Stream qui nous est retournee
+
+		// Maintenant plutot que d'avoir un Stream<> qui contient 4 Stream<> qui contiennent des elements, nous voulons un seul Stream<> avec tous les elements des Stream<Stream<>> remis a plat
+		long countwords = Files.lines(path) // Retourne un Stream<String>
+			.flatMap(split) // Retourne aussi un Stream<String>
+			.count();
+		System.out.println("Count words : " + countwords);
+		// Ainsi flatMap() prends une fonction et cette fonction doit retourner un Stream<>, et elle prends les Stream<> créés par cette fonction et les met a plat dans un Stream<> unique
+		// Donc map() ne change pas le nombre d'elements du Stream<>, mais flatMap() si. 		
+		
+		
+		
+		
+		
+		
 		
 	}
 
