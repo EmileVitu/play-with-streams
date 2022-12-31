3ce3930e532b9f2345de1ca157fe5bcc093ef357
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2022-12-31 16:05:03 +0100

    Created new class with new List<String>

---
+++ play-with-streams/src/org/vitu/stream/CountingStreamElement.java
@@ -0,0 +1,16 @@
+package org.vitu.stream;
+
+import java.util.List;
+
+public class CountingStreamElement {
+	
+	public static void main(String [] args) {
+		
+		List<String> strings = 
+				List.of("one", "five", "one", "two", "six", "seven", "eight", "two", "three", "four",
+						"five", "one", "eight", "nine", "ten", "two", "three", "four", "six", "seven", 
+						"eight", "two", "three", "four", "five", "one", "eight", "nine", "ten", "two", 
+						"one", "eight", "nine", "ten", "eight", "nine", "ten", "nine", "ten", "two");
+		
+	}
+}
