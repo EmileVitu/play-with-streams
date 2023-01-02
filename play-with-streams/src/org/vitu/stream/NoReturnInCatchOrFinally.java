1363aaff6edadb04306d6b4da13bd99c0096d841
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2023-01-02 18:55:33 +0100

    tested stuff on try / catch / finally

---
+++ play-with-streams/src/org/vitu/stream/NoReturnInCatchOrFinally.java
@@ -0,0 +1,18 @@
+package org.vitu.stream;
+
+public class NoReturnInCatchOrFinally {
+
+	public static void main(String[] args) {
+		
+		String quizz = new String{
+			try {
+				return "try";
+			} catch (Exception e) {
+				return "catch";
+			} finally {
+				return "finally";
+			}
+		}
+	}
+
+}
